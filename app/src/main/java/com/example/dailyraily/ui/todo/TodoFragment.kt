a03e47be8d5824b5c9c88a43d3b7dfdea6b9c3be
Sonnet-Songbird <147113185+Sonnet-Songbird@users.noreply.github.com>
2024-02-15 16:07:41 +0900

    Rename: Rename Fragments

--- app/src/main/java/com/example/dailyraily/ui/todo/TodoFragment.kt
+++ app/src/main/java/com/example/dailyraily/ui/todo/TodoFragment.kt
@@ -1,4 +1,4 @@
-package com.example.dailyraily.ui.gallery
+package com.example.dailyraily.ui.todo
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.dailyraily.databinding.FragmentGalleryBinding
+import com.example.dailyraily.databinding.FragmentTodoBinding
 
-class GalleryFragment : Fragment() {
+class TodoFragment : Fragment() {
 
-    private var _binding: FragmentGalleryBinding? = null
+    private var _binding: FragmentTodoBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,14 +22,14 @@ class GalleryFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val galleryViewModel =
-            ViewModelProvider(this).get(GalleryViewModel::class.java)
+        val todoViewModel =
+            ViewModelProvider(this).get(TodoViewModel::class.java)
 
-        _binding = FragmentGalleryBinding.inflate(inflater, container, false)
+        _binding = FragmentTodoBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textGallery
-        galleryViewModel.text.observe(viewLifecycleOwner) {
+        val textView: TextView = binding.textTodo
+        todoViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
--- app/src/main/java/com/example/dailyraily/ui/todo/TodoViewModel.kt
+++ app/src/main/java/com/example/dailyraily/ui/todo/TodoViewModel.kt
@@ -1,13 +1,13 @@
-package com.example.dailyraily.ui.gallery
+package com.example.dailyraily.ui.todo
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class GalleryViewModel : ViewModel() {
+class TodoViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is gallery Fragment"
+        value = "This is Todo Fragment"
     }
     val text: LiveData<String> = _text
 }
