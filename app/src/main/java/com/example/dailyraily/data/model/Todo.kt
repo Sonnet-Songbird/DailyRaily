09a983a74dc3e0d9d1af3932e141ba6993e0a8ad
Sonnet-Songbird <147113185+Sonnet-Songbird@users.noreply.github.com>
2024-02-16 13:30:35 +0900

    FEAT: Add assessment of reset target that based abstract enum method

--- app/src/main/java/com/example/dailyraily/data/model/Todo.kt
+++ app/src/main/java/com/example/dailyraily/data/model/Todo.kt
@@ -3,39 +3,135 @@ package com.example.dailyraily.data.model
 import java.time.LocalDate
 import java.util.UUID
 
-class Todo(private val game: Game, private val name: String, private val id: UUID) {
-    private var gameName = game.name
+class Todo(private val game: Game, private var name: String, private val id: UUID) {
+    private val gameName = game.name
     private var goal: Int = 0
     private var count: Int = 0
-    private var recentReset: LocalDate = game.adjustedDate()
+    private var recentResetDate: LocalDate = game.adjustedDate()
     private lateinit var resetType: ResetType
+    private var important = false
+    val done: Boolean
+        get() = goal <= count
+    val priority: Int
+        get() = resetType.getPriority(this)
+    val resetPending: Boolean
+        get() = resetType.isResetPending(this)
+
 
     init {
         game.register(this, id)
 
     }
 
-    constructor(game: Game, name: String, goal: Int, resetType: ResetType) : this(
+    constructor(
+        game: Game,
+        name: String,
+        goal: Int,
+        resetType: ResetType,
+        important: Boolean
+    ) : this(
         game,
         name,
         UUID.randomUUID()
     ) {
         this.goal = goal
         this.resetType = resetType
+        this.important = important
     }
 
     fun reset() {
         count = 0
-        recentReset = game.adjustedDate()
+        recentResetDate = game.adjustedDate()
     }
 
-    fun resetAll() {}
+    //Todo: Priority 산정 기준 반영.
     enum class ResetType {
-        DAILY,
-        WEEKLY,
-        MONTHLY,
-        NORMAL
+        DAILY {
+            override fun isResetPending(todo: Todo): Boolean {
+                return todo.game.adjustedDate() != todo.recentResetDate
+            }
+
+            override fun getPriority(todo: Todo): Int {
+                return if (todo.done) {
+                    0
+                } else {
+                    if (todo.important)
+                        101
+                    else {
+                        100
+                    }
+                }
+            }
+
+        },
+        WEEKLY {
+            override fun isResetPending(todo: Todo): Boolean {
+                val today = todo.game.adjustedDate()
+                val dateDifference =
+                    (today.dayOfWeek.value - todo.game.resetDOW.value + 7) % 7
+                val resetDate = today.minusDays(dateDifference.toLong());
+
+                return resetDate > todo.recentResetDate
+            }
+
+            override fun getPriority(todo: Todo): Int {
+                return if (todo.done) {
+                    0
+                } else {
+                    if (todo.important)
+                        101
+                    else {
+                        100
+                    }
+                }
+            }
+
+        },
+        MONTHLY {
+            override fun isResetPending(todo: Todo): Boolean {
+                val today = todo.game.adjustedDate()
+                val resetDay = todo.game.resetDay
+                var resetDate = today.withDayOfMonth(resetDay)
+                if (today.dayOfMonth > resetDay)
+                    resetDate = resetDate.minusMonths(1)
+                return resetDate > todo.recentResetDate
+            }
+
+            override fun getPriority(todo: Todo): Int {
+                return if (todo.done) {
+                    0
+                } else {
+                    if (todo.important)
+                        101
+                    else {
+                        100
+                    }
+                }
+            }
+
+        },
+        NORMAL {
+            override fun isResetPending(todo: Todo): Boolean {
+                return false
+            }
+
+            override fun getPriority(todo: Todo): Int {
+                return if (todo.done) {
+                    0
+                } else {
+                    if (todo.important)
+                        101
+                    else {
+                        100
+                    }
+                }
+            }
+        };
+
+        abstract fun isResetPending(todo: Todo): Boolean
+        abstract fun getPriority(todo: Todo): Int
     }
+
 
     override fun toString(): String {
         return name
@@ -53,5 +149,6 @@ class Todo(private val game: Game, private val name: String, private val id: UUI
     override fun hashCode(): Int {
         return id.hashCode()
     }
+
 
 }
