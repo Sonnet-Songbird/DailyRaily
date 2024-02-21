1e99b7e2fcde858c51917a19c394c687abc12927
Sonnet-Songbird <147113185+Sonnet-Songbird@users.noreply.github.com>
2024-02-21 16:59:58 +0900

    FEAT: Add Interface Listable

--- app/src/main/java/com/example/dailyraily/ui/list/ListAdapter.kt
+++ app/src/main/java/com/example/dailyraily/ui/list/ListAdapter.kt
@@ -7,26 +7,37 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.dailyraily.R
 
-//class ListAdapter(private val data: ) :
-//    RecyclerView.Adapter<ListAdapter.ViewHolder>() {
-//
-//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-//        val layoutRes = R.layout.item_layout
-//        val view = LayoutInflater.from(parent.context).inflate(layoutRes, parent, false)
-//        return ViewHolder(view)
-//    }
-//
-//    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-//        holder.bind(data[position])
-//    }
-//
-//    override fun getItemCount(): Int = data.size
-//
-//    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-//        private val textView: TextView = itemView.findViewById(R.id.textView)
-//
-//        fun bind(item: ) {
-//            textView.text = item.text
-//        }
-//    }
-//}
+class ListAdapter(private val data: List<Listable>) :
+    RecyclerView.Adapter<ListAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val layoutRes = R.layout.item_layout
+        val view = LayoutInflater.from(parent.context).inflate(layoutRes, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(data[position])
+    }
+
+    override fun getItemCount(): Int = data.size
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val textView: TextView = itemView.findViewById(R.id.textView)
+
+        fun bind(item: Listable) {
+            textView.text = item.toListItem().columnOne
+        }
+    }
+}
+
+interface Listable {
+    fun toListItem(): ItemData
+}
+
+data class ItemData(
+    val columnOne: String,
+    val columnTwo: String,
+    val columnThree: String,
+    val id: String
+)
