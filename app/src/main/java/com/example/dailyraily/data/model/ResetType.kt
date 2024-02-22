11fd089cc649cc5d5f3b597013c5ad9896cfc8fe
Sonnet-Songbird <147113185+Sonnet-Songbird@users.noreply.github.com>
2024-02-22 11:00:43 +0900

    FEAT: Add Implement getNextResetDate REFACTOR: Integrate function isResetPending

--- app/src/main/java/com/example/dailyraily/data/model/ResetType.kt
+++ app/src/main/java/com/example/dailyraily/data/model/ResetType.kt
@@ -5,12 +5,17 @@ import java.time.LocalDateTime
 //Todo: Priority 산정 기준 반영.
 enum class ResetType {
     DAILY {
-        override fun getNextResetDateTime(todo: Todo): LocalDateTime? {
-            TODO("Not yet implemented")
+        override fun cycle(localDateTime: LocalDateTime, long: Long): LocalDateTime {
+            return localDateTime.plusDays(long)
         }
 
-        override fun isResetPending(todo: Todo): Boolean {
-            return todo.game.adjustedDate != todo.recentResetDate
+        override fun getNextResetDateTime(todo: Todo): LocalDateTime? {
+            val todayReset = LocalDateTime.now().withHour(todo.game.resetHour).withMinute(0)
+            return if (LocalDateTime.now().isAfter(todayReset)) {
+                todayReset.plusDays(1)
+            } else {
+                todayReset
+            }
         }
 
         override fun getPriority(todo: Todo): Int {
@@ -27,18 +32,17 @@ enum class ResetType {
 
     },
     WEEKLY {
+        override fun cycle(localDateTime: LocalDateTime, long: Long): LocalDateTime {
+            return localDateTime.plusWeeks(long)
+        }
+
         override fun getNextResetDateTime(todo: Todo): LocalDateTime? {
-            TODO("Not yet implemented")
-        }
-
-        override fun isResetPending(todo: Todo): Boolean {
             val today = todo.game.adjustedDate
-            val dateDifference =
-                (today.dayOfWeek.value - todo.game.resetDOW.value + 7) % 7
-            val resetDate = today.minusDays(dateDifference.toLong());
-
-            return resetDate > todo.recentResetDate
+            val dateDifference = (todo.game.resetDOW.value - today.dayOfWeek.value + 7) % 7
+            val nextResetDate = today.plusDays(dateDifference.toLong())
+            return nextResetDate.atTime(todo.game.resetHour, 0)
         }
+
 
         override fun getPriority(todo: Todo): Int {
             return if (todo.done) {
@@ -54,17 +58,20 @@ enum class ResetType {
 
     },
     MONTHLY {
-        override fun getNextResetDateTime(todo: Todo): LocalDateTime? {
-            TODO("Not yet implemented")
+        override fun cycle(localDateTime: LocalDateTime, long: Long): LocalDateTime {
+            return localDateTime.plusMonths(long)
         }
 
-        override fun isResetPending(todo: Todo): Boolean {
-            val today = todo.game.adjustedDate
+        override fun getNextResetDateTime(todo: Todo): LocalDateTime? {
             val resetDay = todo.game.resetDay
-            var resetDate = today.withDayOfMonth(resetDay)
-            if (today.dayOfMonth > resetDay)
-                resetDate = resetDate.minusMonths(1)
-            return resetDate > todo.recentResetDate
+            val today = todo.game.adjustedDate
+            val resetDateTime = today.withDayOfMonth(resetDay).atTime(todo.game.resetHour, 0)
+
+            if (todo.game.adjustedDate.dayOfMonth > todo.game.resetDay) {
+                return resetDateTime.plusMonths(1)
+            } else {
+                return resetDateTime
+            }
         }
 
         override fun getPriority(todo: Todo): Int {
@@ -81,12 +88,12 @@ enum class ResetType {
 
     },
     NORMAL {
-        override fun getNextResetDateTime(todo: Todo): LocalDateTime? {
-            return null
+        override fun cycle(localDateTime: LocalDateTime, long: Long): LocalDateTime {
+            return localDateTime
         }
 
-        override fun isResetPending(todo: Todo): Boolean {
-            return false
+        override fun getNextResetDateTime(todo: Todo): LocalDateTime? {
+            return null
         }
 
         override fun getPriority(todo: Todo): Int {
@@ -102,8 +109,19 @@ enum class ResetType {
         }
     };
 
+
     abstract fun getNextResetDateTime(todo: Todo): LocalDateTime?
-    abstract fun isResetPending(todo: Todo): Boolean
+    fun isResetPending(todo: Todo): Boolean {
+        val resetDateTime = getNextResetDateTime(todo)
+
+        return resetDateTime?.let {
+            val resetDate = cycle(it, -1).toLocalDate()
+            resetDate.isAfter(todo.recentResetDate)
+        } ?: false
+    }
+
+    abstract fun cycle(localDateTime: LocalDateTime, long: Long): LocalDateTime
+
     abstract fun getPriority(todo: Todo): Int
 
     companion object {
